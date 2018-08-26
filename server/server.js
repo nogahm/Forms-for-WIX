d62be5f5b9f15c78d8a9fbd54f1dcc2f7ab3e086
nogah <nogahm@post.bgu.ac.il>
2018-08-26 16:42:49 +0300

    create form works

--- server/server.js
+++ server/server.js
@@ -2,35 +2,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
-app.use(cors());
-
 var DButilsAzure = require('./DButils');
 var morgan = require('morgan');
-var jwt = require('jsonwebtoken');
-var port = 3000;
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+var port = 4000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
 //create a new form
 app.post('/createForm', function(req, res)
 {
     var formName=req.body.formName;
     var formFields=req.body.formFields;
-    var id=1;
+    let id=1;
+    let count=0;
     //get form ID
-    DButilsAzure.execQuery("Select max(FormId) From Forms").then(function(result1){
-        if(result1.length>0)
+    DButilsAzure.execQuery("Select Max(FormId) AS formid from Forms").then(function(result1){
+        if(result1.length > 0)
         {
-            id=result1[0].FormId+1;
+            id=result1[0].formid+1;
         }
 
         //insert new form to Forms table
-        DButilsAzure.execQuery("Insert Into Forms Values('"+id+"', '"+formName+"', 0)").then(function(result2){
+        DButilsAzure.execQuery("INSERT INTO Forms VALUES ('" + id + "', '" + formName + "', '" + count + "')").then(function(result2){
             //insert fields to FormField table
-            for(var i=0;i<formFields.length;i++)
+            for(let i=0;i<formFields.length;i++)
             {
-                DButilsAzure.execQuery("Insert Into FormField Values('"+id+"', '"+formFields[i].fieldName+"', '"+formFields[i].fieldType+"')").then(function(result3){
+                DButilsAzure.execQuery("INSERT INTO FormField VALUES ('" + id + "', '" + formFields[i].fieldName + "', '" + formFields[i].fieldType + "')").then(function(result3){
+                    
                 }).catch(function (err) { res.status(400).send(err); });
             }
             res.sendStatus(200);
-        }).catch(function (err) { res.status(400).send(err); });
-    }).catch(function (err) { res.status(400).send(err); });
+        }).catch(function (err) { res.status(401).send(err); });
+    }).catch(function (err) { res.status(402).send(err); });
 })
