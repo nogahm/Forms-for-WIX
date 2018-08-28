802e137308670623e7f5b3d9793f259169976529
nogah <nogahm@post.bgu.ac.il>
2018-08-28 16:57:54 +0300

    ng-repeat works. checking sent answer to server

--- client/components/submitFormController.js
+++ client/components/submitFormController.js
@@ -2,8 +2,9 @@ angular.module('Forms')
     .controller('submitFormController', ['$scope', '$rootScope','$routeParams', '$http', '$location', function ($scope, $rootScope,$routeParams, $http, $location) {
         let self = this;
         let serverUrl = 'http://localhost:4000/';
-        self.formId=$routeParams.formId;
-        self.formFields=[];
+        self.formId=$routeParams.formId; //the current form
+        self.formFields=[]; //field from DB
+        self.result={}; //the form submission
 
         //get all form fields
         $http.get(serverUrl + "formFields/"+self.formId).then(function (res) {
@@ -12,5 +13,28 @@ angular.module('Forms')
             alert('Form Not Found');
         }
         );
+
+        //save form submission on DB
+        self.saveInput=function(){
+            //prepare object in order to sent to server
+            self.result.formId=self.formId;
+            self.result.submit=[];
+            for(let i=0;i<self.formFields.length;i++)
+            {
+                self.result.submit[i]={"fieldName":self.formFields[i].fieldName,
+                                        "value":self.formFields[i].value};
+                // self.result.submit[i].fieldName=self.formFields[i].fieldName;
+                // self.result.submit[i].value=self.formFields[i].value;
+            }
+
+            //save the answer using the server
+            $http.put(serverUrl + "addSubmission",self.result).then(function (res) {
+                alert('Submited');
+                $location.path("/formsList");
+            }, function (res) {
+                alert('Submission Failed');
+            }
+            );
+        }
 
     }]);
