9d6e120369a5186e6ab7e9ae4ca21283381ec523
nogah <nogahm@post.bgu.ac.il>
2018-08-29 13:05:11 +0300

    add README

--- client/components/formSubmissionsController.js
+++ client/components/formSubmissionsController.js
@@ -5,7 +5,7 @@ angular.module('Forms')
         self.formId = $routeParams.formId; //the current form
         self.formFields = []; //field from DB
         self.submissions = [];
-
+        self.subArr = [];
         //get all form fields
         $http.get(serverUrl + "formFields/" + self.formId).then(function (res) {
             self.formFields = res.data;
@@ -16,18 +16,25 @@ angular.module('Forms')
 
         //get all form submissions into array
         $http.get(serverUrl + "formSubmission/" + self.formId).then(function (res) {
-            for (let i = 0; i < res.data.length; i++) {
-                let subId = res.data[i].SubmissionId;
-                let fieldName = res.data[i].FieldName;
-                if (!self.submissions[subId]) {
+            let formsub = res.data;
+            for (let i = 0; i < formsub.length; i++) {
+                let subId = formsub[i].SubmissionId;
+                let fieldName = formsub[i].FieldName;
+                if (typeof self.submissions[subId] == "undefined") {
                     self.submissions[subId] = [];
                 }
-                self.submissions[subId][fieldName] = res.data[i].InputValue;
+                (self.submissions[subId])[fieldName] = formsub[i].InputValue;
+            }
+            //copy to array
+            let index = 0;
+            for (let i = 0; i < self.submissions.length; i++) {
+                if (typeof self.submissions[i] !== "undefined") {
+                    self.subArr[index] = self.submissions[i];
+                    index++;
+                }
             }
         }, function (res) {
             alert('Form Submission Not Found');
         }
         );
-
-
     }]);
